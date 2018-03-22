7b696b10d1530148f48935dc914e654412a581e9
Julius Tens <mail@juliustens.eu>
2018-03-23 00:25:47 +0100

    fix #9 :bug:, shorten test descriptions

--- test.js
+++ test.js
@@ -4,8 +4,11 @@ const tape = require('tape')
 const isEqual = require('lodash.isequal')
 const isDate = require('lodash.isdate')
 const validate = require('validate-fptf')
+const moment = require('moment-timezone')
 
 const meinfernbus = require('./index')
+
+const when = moment.tz('Europe/Berlin').day(10).startOf('day').add('6', 'hour').toDate() // next tuesday, 06:00
 
 tape('meinfernbus.stations', (t) => {
 	meinfernbus.stations().then((s) => {
@@ -48,18 +51,40 @@ tape('meinfernbus.regions', (t) => {
 
 const isBerlin = (s) => (s.type==='station' && s.name.substr(0, 6) === 'Berlin')
 const isFrankfurt = (s) => (s.type==='station' && s.name.substr(0, 9) === 'Frankfurt')
+const isStuttgart = (s) => (s.type==='station' && s.name.substr(0, 9) === 'Stuttgart')
 
-tape('meinfernbus.journeys', (t) => {
-	meinfernbus.journeys({id: '88', type: 'region'}, {id: '96', type: 'region'}).then((j) => {
+tape('meinfernbus.journeys bus', (t) => {
+	// Berlin -> Frankfurt
+	meinfernbus.journeys({id: '88', type: 'region'}, {id: '96', type: 'region'}, when).then((j) => {
 		t.ok(j.length > 1, 'journeys length')
 
 		for(let journey of j) validate(journey)
 
 		const journey = j[0]
-		t.ok(isBerlin(journey.legs[0].origin), 'journeys journey leg origin')
-		t.ok(isFrankfurt(journey.legs[journey.legs.length-1].destination), 'journeys journey leg destination')
-		t.ok(journey.legs.every(l => l.operator.id === 'mfb'), 'journeys journey legs operator id')
-		t.ok(journey.price.currency === 'EUR', 'journey price currency')
+		t.ok(isBerlin(journey.legs[0].origin), 'leg origin')
+		t.ok(isFrankfurt(journey.legs[journey.legs.length-1].destination), 'leg destination')
+		t.ok(journey.legs.every(l => l.mode === 'bus'), 'leg mode')
+		t.ok(journey.legs.every(l => l.operator.id === 'mfb'), 'leg operator id')
+		t.ok(journey.price.currency === 'EUR', 'price currency')
+		t.end()
+	})
+})
+
+tape('meinfernbus.journeys train', (t) => {
+	// Berlin -> Stuttgart
+	meinfernbus.journeys({id: '88', type: 'region'}, {id: '101', type: 'region'}, when).then((j) => {
+		t.ok(j.length > 1, 'journeys length')
+
+		for(let journey of j) validate(journey)
+
+		// journey with 1 leg, starting at Berlin-Lichtenberg
+		const journey = j.find(x => x.legs.length === 1 && x.legs[0].origin.id === '20718')
+		t.ok(!!journey, 'journey')
+		t.ok(isBerlin(journey.legs[0].origin), 'leg origin')
+		t.ok(isStuttgart(journey.legs[journey.legs.length-1].destination), 'leg destination')
+		t.ok(journey.legs.every(l => l.mode === 'train'), 'leg mode')
+		t.ok(journey.legs.every(l => l.operator.id === 'leoloco'), 'leg operator id')
+		t.ok(journey.price.currency === 'EUR', 'price currency')
 		t.end()
 	})
 })
