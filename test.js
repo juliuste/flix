fcc78408ca0a81e7e46025348ea658e0b208e15a
Julius Tens <mail@juliustens.eu>
2018-11-01 06:14:55 +0100

    tests -> async/await, lint tests, soothe the linter

--- test.js
+++ test.js
@@ -10,83 +10,79 @@ const flix = require('.')
 
 const when = moment.tz('Europe/Berlin').day(11).startOf('day').add('13', 'hour').toDate() // next thursday, 13:00
 
-tape('flix.stations', (t) => {
-	flix.stations().then((s) => {
-		t.ok(s.length > 30, 'stations length')
+tape('flix.stations', async t => {
+	const stations = await flix.stations()
 
-		for(let station of s) validate(station)
+	t.ok(stations.length > 30, 'stations length')
+	for (let station of stations) validate(station)
 
-		const berlin = s.filter((x) => x.id === '1')[0]
-		t.ok(berlin.id === '1', 'stations berlin id')
-		t.ok(berlin.name === 'Berlin central bus station', 'stations berlin name')
-		t.ok(berlin.location.street === 'Masurenallee 4-6', 'stations berlin street')
-		t.ok(berlin.location.zip === '14057', 'stations berlin zip')
-		t.ok(berlin.location.address === 'Masurenallee 4-6, 14057 Berlin, Germany', 'stations berlin address')
-		t.ok(isEqual(berlin.location.country, {name: 'Germany', code: 'DE'}), 'stations berlin country')
-		t.ok(berlin.aliases.length >= 0, 'stations berlin aliases')
-		t.ok(isEqual(berlin.regions, ['88']), 'stations berlin regions')
-		t.ok(berlin.connections.length > 20, 'stations berlin connections')
-		t.ok(berlin.slug === 'berlin-zob', 'stations berlin slug')
-		t.end()
-	})
+	const berlin = stations.filter((x) => x.id === '1')[0]
+	t.ok(berlin.id === '1', 'stations berlin id')
+	t.ok(berlin.name === 'Berlin central bus station', 'stations berlin name')
+	t.ok(berlin.location.street === 'Masurenallee 4-6', 'stations berlin street')
+	t.ok(berlin.location.zip === '14057', 'stations berlin zip')
+	t.ok(berlin.location.address === 'Masurenallee 4-6, 14057 Berlin, Germany', 'stations berlin address')
+	t.ok(isEqual(berlin.location.country, { name: 'Germany', code: 'DE' }), 'stations berlin country')
+	t.ok(berlin.aliases.length >= 0, 'stations berlin aliases')
+	t.ok(isEqual(berlin.regions, ['88']), 'stations berlin regions')
+	t.ok(berlin.connections.length > 20, 'stations berlin connections')
+	t.ok(berlin.slug === 'berlin-zob', 'stations berlin slug')
+	t.end()
 })
 
-tape('flix.regions', (t) => {
-	flix.regions().then((r) => {
-		t.ok(r.length > 30, 'regions length')
+tape('flix.regions', async t => {
+	const regions = await flix.regions()
 
-		for(let region of r) validate(region)
+	t.ok(regions.length > 30, 'regions length')
+	for (let region of regions) validate(region)
 
-		const berlin = r.filter((x) => x.id === '88')[0]
-		t.ok(berlin.id === '88', 'regions berlin id')
-		t.ok(berlin.name === 'Berlin', 'regions berlin name')
-		t.ok(isEqual(berlin.location.country, {name: 'Germany', code: 'DE'}), 'regions berlin country')
-		t.ok(berlin.class === 'A', 'regions berlin class')
-		t.ok(berlin.stations.length > 4, 'regions berlin stations')
-		t.ok(berlin.connections.length > 20, 'regions berlin connections')
-		t.ok(berlin.slug === 'berlin', 'regions berlin slug')
-		t.end()
-	})
+	const berlin = regions.filter((x) => x.id === '88')[0]
+	t.ok(berlin.id === '88', 'regions berlin id')
+	t.ok(berlin.name === 'Berlin', 'regions berlin name')
+	t.ok(isEqual(berlin.location.country, { name: 'Germany', code: 'DE' }), 'regions berlin country')
+	t.ok(berlin.class === 'A', 'regions berlin class')
+	t.ok(berlin.stations.length > 4, 'regions berlin stations')
+	t.ok(berlin.connections.length > 20, 'regions berlin connections')
+	t.ok(berlin.slug === 'berlin', 'regions berlin slug')
+	t.end()
 })
 
-const isBerlin = (s) => (s.type==='station' && s.name.substr(0, 6) === 'Berlin')
-const isFrankfurt = (s) => (s.type==='station' && s.name.substr(0, 9) === 'Frankfurt')
-const isStuttgart = (s) => (s.type==='station' && s.name.substr(0, 9) === 'Stuttgart')
+const isBerlin = s => (s.type === 'station' && s.name.substr(0, 6) === 'Berlin')
+const isFrankfurt = s => (s.type === 'station' && s.name.substr(0, 9) === 'Frankfurt')
+const isStuttgart = s => (s.type === 'station' && s.name.substr(0, 9) === 'Stuttgart')
 
-tape('flix.journeys bus', (t) => {
+tape('flix.journeys bus', async t => {
 	// Berlin -> Frankfurt
-	flix.journeys({id: '88', type: 'region'}, {id: '96', type: 'region'}, when).then((j) => {
-		t.ok(j.length > 1, 'journeys length')
+	const journeys = await flix.journeys({ id: '88', type: 'region' }, { id: '96', type: 'region' }, when)
 
-		for(let journey of j) validate(journey)
+	t.ok(journeys.length > 1, 'journeys length')
+	for (let journey of journeys) validate(journey)
 
-		const journey = j[0]
-		t.ok(isBerlin(journey.legs[0].origin), 'leg origin')
-		t.ok(isFrankfurt(journey.legs[journey.legs.length-1].destination), 'leg destination')
-		t.ok(journey.legs.every(l => l.mode === 'bus'), 'leg mode')
-		t.ok(journey.legs.every(l => l.operator.id === 'mfb'), 'leg operator id')
-		t.ok(journey.price.currency === 'EUR', 'price currency')
-		t.ok(isURL(journey.price.url), 'price url')
-		t.end()
-	})
+	const [journey] = journeys
+	t.ok(isBerlin(journey.legs[0].origin), 'leg origin')
+	t.ok(isFrankfurt(journey.legs[journey.legs.length - 1].destination), 'leg destination')
+	t.ok(journey.legs.every(l => l.mode === 'bus'), 'leg mode')
+	t.ok(journey.legs.every(l => l.operator.id === 'mfb'), 'leg operator id')
+	t.ok(journey.price.currency === 'EUR', 'price currency')
+	t.ok(isURL(journey.price.url), 'price url')
+	t.end()
 })
 
-tape('flix.journeys train', (t) => {
+tape('flix.journeys train', async t => {
 	// Berlin -> Stuttgart
-	flix.journeys({id: '88', type: 'region'}, {id: '101', type: 'region'}, when).then((j) => {
-		t.ok(j.length > 1, 'journeys length')
+	const journeys = await flix.journeys({ id: '88', type: 'region' }, { id: '101', type: 'region' }, when)
 
-		for(let journey of j) validate(journey)
+	t.ok(journeys.length > 1, 'journeys length')
+	for (let journey of journeys) validate(journey)
 
-		// journey with 1 leg, starting at Berlin-Lichtenberg
-		const journey = j.find(x => x.legs.length === 1 && x.legs[0].origin.id === '20718')
-		t.ok(!!journey, 'journey')
-		t.ok(isBerlin(journey.legs[0].origin), 'leg origin')
-		t.ok(isStuttgart(journey.legs[journey.legs.length-1].destination), 'leg destination')
-		t.ok(journey.legs.every(l => l.mode === 'train'), 'leg mode')
-		t.ok(journey.legs.every(l => l.operator.id === 'train'), 'leg operator id')
-		t.ok(journey.price.currency === 'EUR', 'price currency')
-		t.ok(isURL(journey.price.url), 'price url')
-		t.end()
-	})
+	// journey with 1 leg, starting at Berlin-Lichtenberg
+	const journey = journeys.find(x => x.legs.length === 1 && x.legs[0].origin.id === '20718')
+	t.ok(!!journey, 'journey')
+	t.ok(isBerlin(journey.legs[0].origin), 'leg origin')
+	t.ok(isStuttgart(journey.legs[journey.legs.length - 1].destination), 'leg destination')
+	t.ok(journey.legs.every(l => l.mode === 'train'), 'leg mode')
+	t.ok(journey.legs.every(l => l.operator.id === 'train'), 'leg operator id')
+	t.ok(journey.price.currency === 'EUR', 'price currency')
+	t.ok(isURL(journey.price.url), 'price url')
+	t.end()
 })
